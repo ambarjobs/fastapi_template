a60d314028a1e3173c1c29f30dfb8de0cdab52ef
Armando M. Baratti <ambarjobs@gmail.com>
2026-02-06 20:25:53 -0300

    Integration tests.

--- src/fastapi_template/main.py
+++ src/fastapi_template/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, Response, status
 from pydantic import ValidationError
 
-from .models.output import HealthCheck, HealthStatus, ValidationErrorModel
+from fastapi_template.models.output import HealthCheck, HealthStatus, ValidationErrorModel
 
 app = FastAPI()
 
+# To facilitate mocking in tests.
+health_check_params = {"status": HealthStatus.OK}
 
 @app.get(
     "/health-check",
@@ -15,9 +17,7 @@ def health_check(response: Response) -> HealthCheck | ValidationErrorModel:
     "Simple health check endpoint."
 
     try:
-        return HealthCheck(status=HealthStatus.OK)
-        # return HealthCheck(status=HealthStatus.OK, spurious=123)
-        # return HealthCheck(status=123)
+        return HealthCheck(**health_check_params)
     except ValidationError as e:
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
         return ValidationErrorModel(title=e.title, error_count=e.error_count(), errors=e.errors())
---
+++ tests/integration/test_main.py
@@ -0,0 +1,71 @@
+import pytest
+from fastapi import status
+from fastapi.testclient import TestClient
+
+import fastapi_template.main as main
+from fastapi_template.main import app
+from fastapi_template.models.output import HealthStatus
+
+
+client = TestClient(app=app)
+
+
+class TestHealthCheck:
+    def test_health_check_endpoint__general_case(self) -> None:
+        expected_response = {"status": "ok"}
+
+        response = client.get(url="/health-check")
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == expected_response
+
+    def test_health_check_endpoint__invalid_output_field(self, monkeypatch) -> None:
+        invalid_status_field = 123
+        expected_response = {
+            "error_count": 1,
+            "errors": [
+                {
+                    "input": invalid_status_field,
+                    "loc": ["status"],
+                    "msg": "Input should be an instance of HealthStatus",
+                    "type": "is_instance_of",
+                    "url": "https://errors.pydantic.dev/2.12/v/is_instance_of"
+                }
+            ],
+            "title": "HealthCheck"
+        }
+
+        monkeypatch.setattr(main, "health_check_params", {"status": invalid_status_field})
+        response = client.get(url="/health-check")
+
+
+        assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+        assert response.json() == expected_response
+
+    def test_health_check_endpoint__additional_output_field(self, monkeypatch) -> None:
+        additional_field = "Not existing on the output model"
+        expected_response = {
+            "error_count": 1,
+            "errors": [
+                {
+                    "input": additional_field,
+                    "loc": [
+                        "additional_field"
+                    ],
+                    "msg": "Extra inputs are not permitted",
+                    "type": "extra_forbidden",
+                    "url": "https://errors.pydantic.dev/2.12/v/extra_forbidden"
+                }
+            ],
+            "title": "HealthCheck"
+        }
+
+        monkeypatch.setattr(
+            main,
+            "health_check_params",
+            {"status": HealthStatus.OK, "additional_field": additional_field}
+        )
+        response = client.get(url="/health-check")
+
+        assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+        assert response.json() == expected_response
